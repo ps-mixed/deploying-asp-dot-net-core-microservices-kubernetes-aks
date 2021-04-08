f34b332214e33b57c6e7cd12073a1c780e8144aa
Zeki Kahraman <zeki.kahraman@gmail.com>
2021-04-08 18:25:41 +0300

    Add HealthController

---
+++ GloboTicket/GloboTicket.Client/Controllers/HealthController.cs
@@ -0,0 +1,31 @@
+﻿using GloboTicket.Web.Models;
+using GloboTicket.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GloboTicket.Web.Controllers
+{
+    public class HealthController : Controller
+    {
+        private readonly IEventCatalogService eventCatalogService;
+        private readonly IShoppingBasketService shoppingBasketService;
+        private readonly Settings settings;
+
+        public HealthController(IEventCatalogService eventCatalogService, IShoppingBasketService shoppingBasketService, Settings settings)
+        {
+            this.eventCatalogService = eventCatalogService;
+            this.shoppingBasketService = shoppingBasketService;
+            this.settings = settings;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // we return 200 ok, when we can access the DB categories and events
+            var getCategories = eventCatalogService.GetCategories();
+            var getEvents = eventCatalogService.GetAll();
+            await Task.WhenAll(new Task[] { getCategories, getEvents });
+
+            return Ok();
+        }
+    }
+}
